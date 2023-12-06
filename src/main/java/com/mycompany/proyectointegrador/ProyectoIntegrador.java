7d5abfe3089e0ff862b1402ccfb1c08971d810bb
ezequiel-lope <126516355+ezequiel-lope@users.noreply.github.com>
2023-12-06 20:06:15 -0300

    Update ProyectoIntegrador.java

--- src/main/java/com/mycompany/proyectointegrador/ProyectoIntegrador.java
+++ src/main/java/com/mycompany/proyectointegrador/ProyectoIntegrador.java
@@ -27,15 +27,13 @@ public class ProyectoIntegrador {
       Servicio avast = new Servicio("avast"); Servicio tango = new Servicio("tango");
       Servicio office365 = new Servicio("office 365");
       Servicio linux = new Servicio("linux");
-      System.out.println("Antes de llamar a crear servicio");
       
-      /*control.crearServicio(linux);
+      control.crearServicio(linux);
       control.crearServicio(windows);
       control.crearServicio(avast);
       control.crearServicio(tango);
-      control.crearServicio(office365);*/
-      System.out.println("despues de llamar a crear servicio");
-      System.out.println("Antes de llamar ");
+      control.crearServicio(office365);
+      
       ArrayList <Servicio> servi1 = new ArrayList();
       servi1.add(windows);
       ArrayList <Servicio> servi2 = new ArrayList();
@@ -44,15 +42,13 @@ public class ProyectoIntegrador {
       servi3.add(avast);
       servi3.add(tango);
       servi3.add(office365);
-      System.out.println("despues de llamar");
-        System.out.println("antes de llamar crear clientes");
+    
       Cliente cli1 = new Cliente("Brenda Malnis","34290902","3513298468","roma 120",servi1);
       Cliente cli2 = new Cliente("Marcela Macaluso","10552330","3515301980","patria 1205",servi2);
       Cliente cli3 = new Cliente("Carlos Lopez","36143539","3514161379","sarmiento 830",servi3);
       controllerAC.crearCliente(cli1);
       controllerAC.crearCliente(cli2);
       controllerAC.crearCliente(cli3);
-        System.out.println("despues de llamar crear cliente");
     
       Tecnico tecnico1 = new Tecnico("Martin Frache",0511, windows );
       Tecnico tecnico2 = new Tecnico("Ezequiel Lopez",2102, linux);
@@ -72,7 +68,15 @@ public class ProyectoIntegrador {
      controllerMA.servicioCliente();
      
      controllerRH.EmitirReporte();
-     
+        
+     controllerMA.servicioCliente();
+       
+     controllerMA.servicioCliente();
+       
+     controllerRH.EmitirReporte();
+
+     controllerMA.marcarProblemaResuelto();
+        
      
       
        
